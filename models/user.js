cabd80682c7d928b36efb201334b0174de14e27e
Deepanshu Verma <129099978+who-deepanshu@users.noreply.github.com>
2024-04-16 22:55:23 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+
+// geo-location schema
+const geoSchema = new Schema({
+    type : {
+        type : String,
+        default : "Point"
+    },
+    coordinates : {
+        type : [Number],
+        index : "2dsphere"
+    }
+});
+
+// Schema for user
+const cabDriverSchema = new Schema({
+    name : {
+        type : String,
+        required : [true, 'Name is required']
+    },
+    rank : {
+        type : String,
+    },
+    available : {
+        type : Boolean,
+        default : false,
+    },
+    geometry : geoSchema
+});
+
+
+// geo-model
+const CabDriver = mongoose.model('cabDriver', cabDriverSchema);
+
+module.exports = CabDriver;
